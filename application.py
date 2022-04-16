dfcbbbfa2110cf933475cd3097b826db366a478a
Ramesan PP <84701649+RamesanPP@users.noreply.github.com>
2022-04-16 10:02:38 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,39 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+        # Access form data
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        # Insert data into database
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month,day)
+
+        # Go back to homepage
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+        # Query for all birthdays
+        birthdays = db.execute("SELECT * FROM birthdays")
+
+        return render_template("index.html", birthdays = birthdays)
+
+
